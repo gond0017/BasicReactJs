3a35d812d8308d4af18d1750cd6f3c8c9acc1774
Sneha <gond0017@algonquinlive.com>
2020-10-07 17:59:40 -0400

    Updates

---
+++ precache-manifest.216cb9d22c331bb553e460ffd07c7de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9366a626c4109373fb33cfe119dab022",
+    "url": "/BasicReactJs/index.html"
+  },
+  {
+    "revision": "8ee120ae0c80a1740348",
+    "url": "/BasicReactJs/static/css/main.296d9760.chunk.css"
+  },
+  {
+    "revision": "1717c49f093f7a0750c3",
+    "url": "/BasicReactJs/static/js/2.16f9e7fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BasicReactJs/static/js/2.16f9e7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee120ae0c80a1740348",
+    "url": "/BasicReactJs/static/js/main.6b61b9ef.chunk.js"
+  },
+  {
+    "revision": "173343cf2d58f7eaed00",
+    "url": "/BasicReactJs/static/js/runtime-main.04e2d35d.js"
+  }
+]);
